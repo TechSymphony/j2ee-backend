5ea6962d3b85548c6b0d9c80c8b6e5781cd7e926
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-10-01 22:40:09 +0700

    fix: weird bug from spring service annotation and not reading env vars

--- resource_server/src/main/java/com/tech_symfony/resource_server/system/image/ImageService.java
+++ resource_server/src/main/java/com/tech_symfony/resource_server/system/image/ImageService.java
@@ -8,7 +8,9 @@ import java.net.URL;
 
 import javax.imageio.ImageIO;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
@@ -22,29 +24,28 @@ import org.telegram.telegrambots.meta.api.objects.message.Message;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
 public interface ImageService {
-    public String sendImage(MultipartFile image);
-    public File getImage(String image);
+    public String sendImage(MultipartFile image) throws Exception;
+    public File getImage(String image) throws Exception;
 }
 
-@Service
-class TelegramService implements ImageService, LongPollingSingleThreadUpdateConsumer {
-
-    @Value("${TELEGRAM_BOT_TOKEN:not_found}")
-    String botToken;
+class TelegramBot implements LongPollingSingleThreadUpdateConsumer {
 
     private TelegramBotsLongPollingApplication application;
     private TelegramClient client;
 
-    @Value("${TELEGRAM_GROUP:not_found}")
-    String chat_id;
+    private String botToken;
+    private String chatId;
 
-    public TelegramService() throws Exception {
+    public TelegramBot(String botToken, String chatId) throws Exception {
         if (botToken.equals("not_found")) {
             throw new Exception("TELEGRAM_TOKEN not found");
         }
         application = new TelegramBotsLongPollingApplication();
         application.registerBot(botToken, this);
         client = new OkHttpTelegramClient(botToken);
+
+        this.botToken = botToken;
+        this.chatId = chatId;
     }
 
     public File getImage(String image) {
@@ -69,7 +70,7 @@ class TelegramService implements ImageService, LongPollingSingleThreadUpdateCons
             try (OutputStream os = new FileOutputStream(f)) {
                 os.write(image.getBytes());
                 // Sending image...
-                SendPhoto sP = new SendPhoto(chat_id, new InputFile(f));
+                SendPhoto sP = new SendPhoto(chatId, new InputFile(f));
                 Message m = client.execute(sP);
                 f.delete();
                 System.out.println(m.getPhoto().get(m.getPhoto().size() - 1).getFileId());
@@ -82,22 +83,30 @@ class TelegramService implements ImageService, LongPollingSingleThreadUpdateCons
         return null;
     }
 
-    public static void main(String[] args) {
-        try {
-            TelegramService s = new TelegramService();
-
-//            s.sendImage("telegram", "https://i1-vnexpress.vnecdn.net/2024/09/28/Trump-9403-1727483522.png?w=1020&h=0&q=100&dpr=1&fit=crop&s=wu0EE_1OkSaeLPCNK73IxQ");
-
-            System.out.println(s.getImage("AgACAgUAAxkDAAMEZveb6jr5iETdPGp4hMWWKhdLz4IAAnC_MRsj-rlXB5IjdmyTpowBAAMCAANzAAM2BA"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
     @Override
     public void consume(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-			System.out.println(update.getMessage().getText());
-		}
+            System.out.println(update.getMessage().getText());
+        }
+    }
+}
+
+@Service
+class TelegramService implements ImageService {
+    private Environment env;
+
+    @Value("${telegram.bot_token:not_found}")
+    private String botToken;
+    @Value("${telegram.group}")
+    private String chatId;
+
+    @Override
+    public String sendImage(MultipartFile image) throws Exception {
+        return new TelegramBot(botToken, chatId).sendImage(image);
+    }
+
+    @Override
+    public File getImage(String image) throws Exception {
+        return new TelegramBot(botToken, chatId).getImage(image);
     }
 }
