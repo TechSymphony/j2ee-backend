ee07621bcc3e9c3c12b66f87177d02e09a66762b
1119-DuyNguyen <thanhduy191103@gmail.com>
2024-10-12 06:33:36 +0700

    setup migration rbac

---
+++ auth_server/src/main/java/com/tech_symfony/auth_server/model/UserRepository.java
@@ -0,0 +1,12 @@
+package com.tech_symfony.auth_server.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("SELECT u FROM User u WHERE u.email = :username")
+    public User getUserByUsername(@Param("username") String username);
+}
