0fd517789ec51991c58cf381cf1e286e072e1c7b
Edric <datnguyen2132003@gmail.com>
2024-10-19 03:04:20 +0700

    update: add Beneficiary to show CampaignListVm

--- resource_server/src/main/java/com/tech_symfony/resource_server/api/campaign/viewmodel/CampaignListVm.java
+++ resource_server/src/main/java/com/tech_symfony/resource_server/api/campaign/viewmodel/CampaignListVm.java
@@ -1,6 +1,7 @@
 package com.tech_symfony.resource_server.api.campaign.viewmodel;
 
+import com.tech_symfony.resource_server.api.beneficiary.Beneficiary;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-public record CampaignListVm(int id, String name, String description, BigDecimal targetAmount, BigDecimal currentAmount, LocalDate startDate, LocalDate endDate, boolean isApproved, String code ) {
+public record CampaignListVm(int id, String name, String description, BigDecimal targetAmount, BigDecimal currentAmount, LocalDate startDate, LocalDate endDate, boolean isApproved, String code , Beneficiary beneficiary) {
 }
