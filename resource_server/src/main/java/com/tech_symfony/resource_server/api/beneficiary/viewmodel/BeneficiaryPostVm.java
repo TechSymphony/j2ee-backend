9d7fc3bcfde4f0136a07b8ce92ac1d459bcf791d
Edric <datnguyen2132003@gmail.com>
2024-11-06 16:16:18 +0700

    fix: update beneficiary

--- resource_server/src/main/java/com/tech_symfony/resource_server/api/beneficiary/viewmodel/BeneficiaryPostVm.java
+++ resource_server/src/main/java/com/tech_symfony/resource_server/api/beneficiary/viewmodel/BeneficiaryPostVm.java
@@ -1,6 +1,26 @@
 package com.tech_symfony.resource_server.api.beneficiary.viewmodel;
 
-import java.math.BigDecimal;
+import com.tech_symfony.resource_server.api.beneficiary.Beneficiary;
+import com.tech_symfony.resource_server.api.beneficiary.BeneficiaryStatusEnum;
+import com.tech_symfony.resource_server.api.campaign.CampaignsStatusEnum;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.checkerframework.common.aliasing.qual.Unique;
+import org.springframework.lang.Nullable;
 
-public record BeneficiaryPostVm(String situationDetail, BigDecimal supportReceived) {
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public record BeneficiaryPostVm(
+        @Size(max =255)
+        @NotEmpty(message = "Situation-Detail must not be empty")
+        String situationDetail,
+
+        @NotNull(message = "supportReceived cannot be null")
+        BigDecimal supportReceived,
+
+        @NotNull(message = "Status cannot be null")
+        BeneficiaryStatusEnum  verificationStatus)
+{
 }
