4cfb6f3956c37f099436c87f4bed8320d7f3c510
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-11-16 10:13:31 +0700

    feat: setup websocket

---
+++ resource_server/src/main/java/com/tech_symfony/resource_server/system/config/WebSocketBrokerConfig.java
@@ -0,0 +1,25 @@
+package com.tech_symfony.resource_server.system.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic", "/queue", "/all");
+        registry.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // for browser
+        registry.addEndpoint("/ws");
+        registry.addEndpoint("/ws").setAllowedOrigins("*")
+                .setAllowedOrigins("https://_ws.test").withSockJS();
+    }
+}
