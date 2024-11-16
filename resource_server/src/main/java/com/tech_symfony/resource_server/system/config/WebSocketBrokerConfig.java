07f0cc4552402d6dba928f17aaebbf3eceac240f
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-11-16 10:13:31 +0700

    feat: setup websocket

--- resource_server/src/main/java/com/tech_symfony/resource_server/system/config/WebSocketBrokerConfig.java
+++ resource_server/src/main/java/com/tech_symfony/resource_server/system/config/WebSocketBrokerConfig.java
@@ -16,6 +16,7 @@ public class WebSocketBrokerConfig implements WebSocketMessageBrokerConfigurer {
         registry.setUserDestinationPrefix("/user");
     }
 
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // for browser
