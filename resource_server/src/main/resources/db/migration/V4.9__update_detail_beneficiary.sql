a87feda0310451439d34aafa1ababe45792ae821
Edric <94811667+edricnguyen03@users.noreply.github.com>
2024-11-27 21:16:59 +0700

    update: update data detail beneficiary (#48)

---
+++ resource_server/src/main/resources/db/migration/V4.9__update_detail_beneficiary.sql
@@ -0,0 +1,9 @@
+
+UPDATE beneficiaries
+    SET  situation_detail = '<p>Hiện tại với một số khó khăn trong quá trình giải quyết vấn đề học tập, hoặc phát triển kỹ năng chuyên môn. Mặc dù tôi đã cố gắng tìm cách khắc phục, nhưng tôi cảm thấy cần có sự hỗ trợ để vượt qua giai đoạn này một cách hiệu quả hơn. Tôi rất mong nhận được sự giúp đỡ từ cơ quan, tổ chức hoặc cá nhân cụ thể để có thể tiếp tục tiến bước và hoàn thành mục tiêu của mình. Tôi xin chân thành cảm ơn sự quan tâm và giúp đỡ, và hy vọng sẽ nhận được sự hỗ trợ từ quý vị trong thời gian sớm nhất.</p>'
+    WHERE id = 1;
+
+UPDATE beneficiaries
+    SET  situation_detail = '<p>Hiện tại với một số khó khăn trong quá trình giải quyết vấn đề học tập, hoặc phát triển kỹ năng chuyên môn. Mặc dù tôi đã cố gắng tìm cách khắc phục, nhưng tôi cảm thấy cần có sự hỗ trợ để vượt qua giai đoạn này một cách hiệu quả hơn. Tôi rất mong nhận được sự giúp đỡ từ cơ quan, tổ chức hoặc cá nhân cụ thể để có thể tiếp tục tiến bước và hoàn thành mục tiêu của mình. Tôi xin chân thành cảm ơn sự quan tâm và giúp đỡ, và hy vọng sẽ nhận được sự hỗ trợ từ quý vị trong thời gian sớm nhất.</p>'
+    WHERE id = 2;
+
