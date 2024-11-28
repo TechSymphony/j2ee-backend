95f8cccf30da85a4605c8218cd1fb8ec39a3fd96
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-11-28 23:14:05 +0700

    feat: user table (#55)

--- resource_server/src/main/java/com/tech_symfony/resource_server/api/user/viewmodel/UserListVm.java
+++ resource_server/src/main/java/com/tech_symfony/resource_server/api/user/viewmodel/UserListVm.java
@@ -5,6 +5,6 @@ import com.tech_symfony.resource_server.api.role.viewmodel.RoleListVm;
 
 import java.time.Instant;
 
-public record UserListVm(Integer id, String fullName, String email, String phone, Role role, String username, boolean isStudent) {
+public record UserListVm(Integer id, String fullName, String email, String phone, Role role, String username, boolean isStudent, Boolean enabled) {
 
 }
