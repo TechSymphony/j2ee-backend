aac283ce6aeb2e7681a370925eb7f90e79c7226a
Tiền Minh Vy <tienminhvydev@gmail.com>
2024-11-28 13:41:15 +0700

    feat: filter by date range

--- resource_server/src/main/java/com/tech_symfony/resource_server/api/donation/viewmodel/DonationExportVm.java
+++ resource_server/src/main/java/com/tech_symfony/resource_server/api/donation/viewmodel/DonationExportVm.java
@@ -3,10 +3,22 @@ package com.tech_symfony.resource_server.api.donation.viewmodel;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
+import java.time.LocalDate;
+
 public record DonationExportVm(
         @NotNull(message = "Campaign cannot be null")
         int campaign,
 
-        String type
+        @NotNull(message = "From date field cannot be null")
+        LocalDate from,
+
+        @NotNull(message = "To date field cannot be null")
+        LocalDate to,
+
+        @NotNull(message = "Student only field cannot be null")
+        boolean studentOnly,
+
+        @NotNull(message = "Is anonymous field cannot be null")
+        boolean isAnonymous
 ) {
 }
